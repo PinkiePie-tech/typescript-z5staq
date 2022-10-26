bfd03f751f067a5028ac9d49f6c24c2ea2ecaa16
PinkiePie-tech <62440844+PinkiePie-tech@users.noreply.github.com>
2022-10-26 10:46:55 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -34,3 +34,21 @@ function palindromeJeSuis(value: number[]): boolean {
 console.log(palindromeJeSuis(exo2tab1));
 console.log(palindromeJeSuis(exo2tab2));
 console.log(palindromeJeSuis(exo2tab3));
+
+// Un petit exercice facultatif car très complexe ^^ connais tu les nombres premier ? un nombre premier est un nombre qui ne peut être divisible que par 1 et par lui mème.
+// petit exemple, 1 est un nombre premier, il peut être divisé par 1 et par lui même 1
+// 7 est un nombre premier, il ne peut être divisé que par 1 et par lui même 7
+// 6 est un nombre impremier, il peut être divisé par 1, 2, 3, et 6
+
+function premierJeSuis(value: number): boolean {
+  //
+  // code ici :D j'ai même pas l'idée de comment faire ce truc donc c'est vraiment si tu veux te challenger
+  //
+}
+
+console.log(premierJeSuis(1));
+console.log(premierJeSuis(3));
+console.log(premierJeSuis(6));
+console.log(premierJeSuis(7));
+console.log(premierJeSuis(13));
+console.log(premierJeSuis(16));
